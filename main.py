0abdb89c4e88d31dd8b8b062cd1ba1a146948e9a
Adrian Bernardino <65195311+adrianb896@users.noreply.github.com>
2022-10-29 16:57:09 -0700

    update on orphan tags
    git push

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ docRelation = {"HRD":("HRS"), "HRS":("PRS"), "PRS":("URS","RISK"), "HTR":("HTP")
                "SVAL":("BOLUS", "ACE", "AID"), "SVATR":("SVAL"), "UT":("UNIT"), "INS": ("UNIT")}      # to be created by the GUI
 
 # The dictionary holds the tags as Keys and the file where they are found as a ValueError
-docFile = {"HRD":"HDS_new_pump.docx", "HRS":"HRS_new_pump.docx", "HTP":"HTP_new_pump.docx", "HTR":"HTR_new_pump.docx", \
-           "PRS":"PRS_new_pump.docx", "RISK":"RiskAnalysis_Pump.docx", "SDS":"SDS_New_pump_x04.docx", \
-           "ACE":"SRS_ACE_Pump_X01.docx", "BOLUS":"SRS_BolusCalc_Pump_X04.docx", "SRS":"SRS_DosingAlgorithm_X03.docx", \
+docFile = {"HRD":"HDS_new_pump.docx", "HRS":"HRS_new_pump.docx", "HTP":"HTP_new_pump.docx", "HTR":"HTR_new_pump.docx",
+           "PRS":"PRS_new_pump.docx", "RISK":"RiskAnalysis_Pump.docx", "SDS":"SDS_New_pump_x04.docx",
+           "ACE":"SRS_ACE_Pump_X01.docx", "BOLUS":"SRS_BolusCalc_Pump_X04.docx", "SRS":"SRS_DosingAlgorithm_X03.docx",
            "SVAL":"SVaP_new_pump.docx", "SVATR":"SVaTR_new_pump.docx", "UT":"SVeTR_new_pump.docx", "URS":"URS_new_pump.docx"}
 
 docFileList = list(docFile.keys())                         # extracts all the tags into a list
-filePath = "C:/Users/steph/OneDrive/Desktop/Docs_Project/" #location of the directory of all documents
+#filePath = "C:/Users/steph/OneDrive/Desktop/Docs_Project/" #location of the directory of all documents
+filePath = "/Users/adrian/Desktop/SampledocsTandem/"
 
 def GetText(filename):                                 #Extracts all lines of any document and converts it into a list format (paragraph by paragraph)
     doc = docx.Document(filename)
@@ -52,8 +53,41 @@ def GetChildTags():
                 if len(y) != 0:
                     print(y[0])
 
-GetParentTags()
-GetChildTags()
+def orphanChildTags():
+    for tag in docFileList:
+        textList = GetText(filePath + docFile["ACE"])
+        index = 0
+        ind = []
+        for t in textList:
+            if re.search('.*:' + re.escape(tag) + ':', t):
+                ind.append(index)
+                result = re.findall('\S*:TBV:\S*', t)
+                if len(result) != 0:
+                    print("From " + docFile["ACE"])
+                    print("This tag has no parent: ", result[0])
+            else:
+                if re.search('.*:' + re.escape(tag) + ':', t):
+                    ind.append(index)
+                    result1 = re.findall('\S*:DER:1*', t)
+                    if len(result1) != 0:
+                        print("From " + docFile["ACE"])
+                        print("This tag has no parent: ", result1[0])
+    for tag in docFileList:
+        textList = GetText(filePath + docFile[tag])
+        index = 0
+        ind = []
+        for t in textList:
+            if re.search('.*:' + re.escape(tag) + ':', t):
+                ind.append(index)
+                result2 = re.findall('\S*:DER:\S*', t)
+                if len(result2) != 0:
+                    print("From " + docFile["SRS"])
+                    print("This tag has no parent: ", result2[0])
+
+
+#GetParentTags()
+#GetChildTags()
+orphanChildTags()
 
 '''
 #def main():
