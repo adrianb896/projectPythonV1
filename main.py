35a8bb78760671af5b480c505dac60d7aeb2dc66
Adrian Bernardino <65195311+adrianb896@users.noreply.github.com>
2022-10-31 00:53:35 -0700

    in progress
    push commit

--- main.py
+++ main.py
@@ -3,35 +3,43 @@ import re
 import xlwings
 
 # The dictionary holds  the tuples of child tags as Values. Here Keys are all the parent tags
-docRelation = {"HRD":("HRS"), "HRS":("PRS"), "PRS":("URS","RISK"), "HTR":("HTP"), "HTP":("HRD", "HRS"), \
-               "SDS":("BOLUS","ACE","AID"), "ACE":("PRS", "TBV", "DER"), "BOULUS":("PRS"), "AID":("PRS","DER"), \
-               "SVAL":("BOLUS", "ACE", "AID"), "SVATR":("SVAL"), "UT":("UNIT"), "INS": ("UNIT")}      # to be created by the GUI
+docRelation = {"HRD": ("HRS"), "HRS": ("PRS"), "PRS": ("URS", "RISK"), "HTR": ("HTP"), "HTP": ("HRD", "HRS"), \
+               "SDS": ("BOLUS", "ACE", "AID"), "ACE": ("PRS", "TBV", "DER"), "BOULUS": ("PRS"), "AID": ("PRS", "DER"), \
+               "SVAL": ("BOLUS", "ACE", "AID"), "SVATR": ("SVAL"), "UT": ("UNIT"),
+               "INS": ("UNIT")}  # to be created by the GUI
 
 # The dictionary holds the tags as Keys and the file where they are found as a ValueError
-docFile = {"HRD":"HDS_new_pump.docx", "HRS":"HRS_new_pump.docx", "HTP":"HTP_new_pump.docx", "HTR":"HTR_new_pump.docx",
-           "PRS":"PRS_new_pump.docx", "RISK":"RiskAnalysis_Pump.docx", "SDS":"SDS_New_pump_x04.docx",
-           "ACE":"SRS_ACE_Pump_X01.docx", "BOLUS":"SRS_BolusCalc_Pump_X04.docx", "SRS":"SRS_DosingAlgorithm_X03.docx",
-           "SVAL":"SVaP_new_pump.docx", "SVATR":"SVaTR_new_pump.docx", "UT":"SVeTR_new_pump.docx", "URS":"URS_new_pump.docx"}
+docFile = {"HRD": "HDS_new_pump.docx", "HRS": "HRS_new_pump.docx", "HTP": "HTP_new_pump.docx",
+           "HTR": "HTR_new_pump.docx",
+           "PRS": "PRS_new_pump.docx", "RISK": "RiskAnalysis_Pump.docx", "SDS": "SDS_New_pump_x04.docx",
+           "ACE": "SRS_ACE_Pump_X01.docx", "BOLUS": "SRS_BolusCalc_Pump_X04.docx",
+           "SRS": "SRS_DosingAlgorithm_X03.docx",
+           "SVAL": "SVaP_new_pump.docx", "SVATR": "SVaTR_new_pump.docx", "UT": "SVeTR_new_pump.docx",
+           "URS": "URS_new_pump.docx"}
 
-docFileList = list(docFile.keys())                         # extracts all the tags into a list
-#filePath = "C:/Users/steph/OneDrive/Desktop/Docs_Project/" #location of the directory of all documents
+docFileList = list(docFile.keys())  # extracts all the tags into a list
+
+# filePath = "C:/Users/steph/OneDrive/Desktop/Docs_Project/" #location of the directory of all documents
 filePath = "/Users/adrian/Desktop/SampledocsTandem/"
 
-def GetText(filename):                                 #Extracts all lines of any document and converts it into a list format (paragraph by paragraph)
+
+def GetText(filename):  # Extracts all lines of any document and converts it into a list format (paragraph by paragraph)
     doc = docx.Document(filename)
     fullText = []
-    for para in doc.paragraphs:                            #Where the collection is a set of paragraphs extracted from the document
+    for para in doc.paragraphs:  # Where the collection is a set of paragraphs extracted from the document
         fullText.append(para.text)
-    fullText = [ele for ele in fullText if ele.strip()]    # returns elements, does four loop  and if statement strips white spacesand storing back to list
-    return fullText                                        # returns everything back into full text into list
+    fullText = [ele for ele in fullText if
+                ele.strip()]  # returns elements, does four loop  and if statement strips white spacesand storing back to list
+    return fullText  # returns everything back into full text into list
+
 
 def GetParentTags():
     for tag in docFileList:
-        textList = GetText(filePath + docFile[tag])         #runsrpevous function
+        textList = GetText(filePath + docFile[tag])  # runsrpevous function
         index = 0
         ind = []
         for t in textList:
-            if re.search('.*:' + re.escape(tag) + ':', t): #conduct regular expression search
+            if re.search('.*:' + re.escape(tag) + ':', t):  # conduct regular expression search
                 ind.append(index)
                 tt = t
                 y = re.findall('\S*:' + re.escape(tag) + ':\S*', t)
@@ -53,41 +61,41 @@ def GetChildTags():
                 if len(y) != 0:
                     print(y[0])
 
-def orphanChildTags():
-    for tag in docFileList:
-        textList = GetText(filePath + docFile["ACE"])
-        index = 0
-        ind = []
-        for t in textList:
-            if re.search('.*:' + re.escape(tag) + ':', t):
-                ind.append(index)
-                result = re.findall('\S*:TBV:\S*', t)
-                if len(result) != 0:
-                    print("From " + docFile["ACE"])
-                    print("This tag has no parent: ", result[0])
-            else:
-                if re.search('.*:' + re.escape(tag) + ':', t):
-                    ind.append(index)
-                    result1 = re.findall('\S*:DER:1*', t)
-                    if len(result1) != 0:
-                        print("From " + docFile["ACE"])
-                        print("This tag has no parent: ", result1[0])
+
+docRelationList = list(docRelation.values())
+docRelationSet = set(docRelationList)
+uniqueList = (list(docRelationSet))
+print("Unique List: ", uniqueList, "\n")
+
+
+def validTags():
     for tag in docFileList:
         textList = GetText(filePath + docFile[tag])
         index = 0
         ind = []
         for t in textList:
-            if re.search('.*:' + re.escape(tag) + ':', t):
+            if re.search('.*:' + re.escape(tag) + ':', t):  # conduct regular expression search
                 ind.append(index)
-                result2 = re.findall('\S*:DER:\S*', t)
-                if len(result2) != 0:
-                    print("From " + docFile[tag])
-                    print("This tag has no parent: ", result2[0])
+                tt = t
+                y = re.findall('\s\[.+\]\s', t)
+                if len(y) != 0:
+                    valid = y not in uniqueList
+                    print(valid, y[0])
+                elif len(y) == 0:
+                    notValid = uniqueList in y
+                    print(notValid, uniqueList)
+                    # elInSet2 = uniqueList not in textList
+                    # print(elInSet2)
+        # for x in textList:
+        #     if x in uniqueList:
+        #         print("Valid")
+        #     elif x not in uniqueList:
+        #         print("Not valid")
 
 
-#GetParentTags()
-#GetChildTags()
-orphanChildTags()
+validTags()
+# GetParentTags()
+# GetChildTags()
 
 '''
 #def main():
